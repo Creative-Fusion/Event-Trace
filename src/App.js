5fc9447a27fe09142fe4ee9bad6d0c939a21bb54
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2022-02-15 16:57:03 +0545

    navbar modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./screens/home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <span className='bg-gray-200'>EventTrace Checked</span>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="relative App">
+				<NavBar loggedIn={true} />
+				<div className="content">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/screens/home.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export const Home = () => {
+	return (
+		<div>
+			<header className="App-header h-screen">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<span className="bg-gray-200">EventTrace Checked</span>
+			</header>
+			<header className="App-header h-screen">
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<span className="bg-gray-200">EventTrace Checked</span>
+			</header>
+		</div>
+	);
+};
