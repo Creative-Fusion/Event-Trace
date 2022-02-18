63b381a6576692ee42c63fa587512a23753af4b1
shreya2057 <shreya2057@gmail.com>
2022-02-18 20:02:44 +0545

    User Profile screen

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { NavBar } from "./components/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./screens/home";
 import { CreateEvent } from "./screens/CreateEvent";
+import UserProfile from "./screens/UserProfile";
 
 function App() {
 	return (
 		<Router>
 			<div className="relative App">
-				<NavBar loggedIn={false} />
+				<NavBar loggedIn={true} />
 				<div className="content">
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/events/create" element={<CreateEvent />}></Route>
+						<Route path="/user/userprofile" element={<UserProfile />}></Route>
 					</Routes>
 				</div>
 			</div>
