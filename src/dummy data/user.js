c9aa065e568745ee2c3c98f2e5b10b8d154da846
shreya2057 <shreya2057@gmail.com>
2022-03-17 15:47:27 +0545

    Merge branch 'master' of https://github.com/Creative-Fusion/Event-Trace into user-profile

---
+++ src/dummy data/user.js
@@ -0,0 +1,10 @@
+const user={
+    name: "John William",
+    email: "johnwilliam@gmail.com",
+    contactNo: "+977 9861252346",
+    address: "Kathmandu, Nepal",
+    gender: "Male",
+    userImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_d3SP2vKOeGFVESn5rk6xnPiQ0naW2e-ldA&usqp=CAU"
+}
+
+export default user;
--- src/screens/user/UserDashboard.js
+++ src/screens/user/UserDashboard.js
@@ -1,16 +1,12 @@
+import EventList from "../../components/EventList";
 import SideBar from "../../components/SideBar";
+import user from "../../dummy data/user";
 import UserProfile from "./UserProfile";
 
-const Userdashboard = () => {
 
-    const user={
-        name: "John Williams",
-        email: "johnwilliam@gmail.com",
-        contactNo: "+977 9861252346",
-        address: "Kathmandu, Nepal",
-        gender: "Male",
-        userImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_d3SP2vKOeGFVESn5rk6xnPiQ0naW2e-ldA&usqp=CAU"
-    }
+
+const Userdashboard = () => {
+    
 
     return ( 
         <div className="absoulte flex flex-wrap flex-row m-0 font-sans w-screen">
@@ -18,8 +14,12 @@ const Userdashboard = () => {
                 <div className="hidden lg:block xl:basis-[21%] lg:basis-[19%]">
                     <SideBar user= { user }/>
                 </div>
-                <div className="grow">
+                {/* <div className="grow">
                     <UserProfile user={ user }/>
+                </div> */}
+
+                <div className="grow">
+                    <EventList/>
                 </div>
                 
             </div>
