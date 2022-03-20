5f872fef5fb171ecf8dc6839c416c70060ba9bb0
Nista Simkhada <58097961+nistasimkhada977@users.noreply.github.com>
2022-03-20 06:24:45 +0545

    participants list incompelete

--- src/screens/event/eventDescription/Participants.js
+++ src/screens/event/eventDescription/Participants.js
@@ -1,5 +1,130 @@
 import React from "react";
 
 export const Participants = () => {
-	return <div className="text-bold text-lg">Participants</div>;
+	return (
+		<div className="drop-shadow-md" h-80 w-80 ml-64>
+            <h2>"ParticipantsList"</h2>
+<table class="table-auto p-4 bg-white shadow rounded-lg ml-72">
+    <thead>
+        <tr>
+            <th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                #
+            </th>
+            <th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+               Name
+            </th>
+            <th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Email
+            </th>
+            <th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+                Username
+            </th>
+			<th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+               Address
+            </th>
+			<th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+              Phone
+            </th>
+			<th class="border p-4 dark:border-dark-5 whitespace-nowrap font-normal text-gray-900">
+               Actions
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="text-gray-700">
+            <td class="border p-4 dark:border-dark-5">
+               1
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+             Nista 
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               nisasimkh100@gmail.com
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               Nishu
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               Thali
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               983456234
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               registered
+               </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border p-4 dark:border-dark-5">
+              2
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+         
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+              
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+        
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border p-4 dark:border-dark-9">
+              
+            </td>
+            <td class="border p-4 dark:border-dark-9">
+               
+            </td>
+            <td class="border p-4 dark:border-dark-9">
+             
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+           
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+        </tr>
+        <tr class="text-gray-700">
+            <td class="border p-4 dark:border-dark-5">
+               
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+                
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+              
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               
+            </td>
+            <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+               <td class="border p-4 dark:border-dark-5">
+               
+               </td>
+        </tr>
+    </tbody>
+</table>
+</div>
+	);
 };
